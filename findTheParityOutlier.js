0af6230d153ae8fe4174a4035cb09216f1976662
Maria Cristina Del Valle <32019417+mardelvalle@users.noreply.github.com>
2018-05-23 04:04:32 -0700

    Create findTheParityOutlier.js
    You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.

---
+++ findTheParityOutlier.js
@@ -0,0 +1,18 @@
+/* You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.*/
+function findOutlier(integers){
+  //your code here
+  if(((integers[0] % 2 == 0) && (integers[1]% 2 == 0)) || ((integers[1] % 2 == 0) && (integers[2]% 2 == 0)) || ((integers[0] % 2 == 0) && (integers[2] % 2 == 0))) {
+    for (let i = 0; i<integers.length; i++){
+      if (integers[i] % 2 !== 0){
+        return (integers[i])
+      }
+    }
+  }
+  else {
+    for (let i = 0; i<integers.length; i++){
+      if (integers[i] % 2 == 0){
+        return (integers[i])
+      }
+    }
+  }
+}
